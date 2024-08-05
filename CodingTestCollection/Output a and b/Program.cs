25b65671a260875dda9cebecb32f806b3143b74e
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-08-05 18:06:14 +0900

    A와 B 출력하기
    입력 값을 띄어쓰기를 기준으로 나눠 배열에 입력하여 따로 출력한다.

---
+++ CodingTestCollection/Output a and b/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Output_a_and_b
+{
+    /// <summary>
+    /// 문제 번호 : 181951
+    /// 문제 이름 : a와 b 출력하기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/181951
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ab1 = Console.ReadLine();
+            string[] ab2 = ab1.Split(' ');
+
+            Console.WriteLine($"a = {ab2[0]}");
+            Console.WriteLine($"b = {ab2[1]}");
+        }
+    }
+}
