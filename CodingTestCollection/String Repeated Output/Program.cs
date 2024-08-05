9dc560f1139093c39ee916d3095afab40c9f7b18
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-08-05 17:00:56 +0900

    문제 번호 : 181950
    문제 : 문자열 반복해서 출력하기

---
+++ CodingTestCollection/String Repeated Output/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace String_Repeated_Output
+{
+    /// <summary>
+    /// 문제 번호 : 181950
+    /// 문제 : 문자열 반복해서 출력하기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/181950
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
