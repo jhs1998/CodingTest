1d3f6448bdc697a2dcd584e4531581e74431a538
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-08-05 17:30:31 +0900

    문자열 출력하기
    문자열 출력하기 해결

--- CodingTestCollection/String Repeated Output/Program.cs
+++ CodingTestCollection/String Repeated Output/Program.cs
@@ -8,7 +8,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string str = Console.ReadLine();
+            Console.WriteLine($"{str}");
         }
     }
 }
--- CodingTestCollection/StringOutput/Program.cs
+++ CodingTestCollection/StringOutput/Program.cs
@@ -9,7 +9,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string str = Console.ReadLine();
+            Console.WriteLine($"{str}");
         }
     }
 }
