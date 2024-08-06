b387e3108de0a0abd2184801ab89f36da3e3a9f0
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-08-06 17:28:00 +0900

    과일 장수 문제
    문제 번호 : 135808
    배열을 정렬한 후 배열의 끝까지 돌때마다 현재 위치에서 m 만큼 왼쪽으로 이동 그 위치의 값을 m과 곱해 합산

---
+++ CodingTestCollection/CorrectParenthesis/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FruitVendor
+{
+    /// <summary>
+    /// 문제 번호 : 135808
+    /// 문제 이름 : 과일 장수
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/135808
+    /// </summary>
+    public class Solution
+    {
+        public int solution(int k, int m, int[] score)
+        {
+            int answer = 0;
+
+            Array.Sort(score);
+
+            for (int i = score.Length; i >= m; i -= m)
+            {
+                answer += m * score[i - m];
+            }
+            return answer;
+        }
+    }
+}
