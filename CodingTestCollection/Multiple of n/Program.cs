3f0fb15625282d1946ad429e505634edf34890cf
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-08-07 18:08:13 +0900

    n의 배수
    값을 띄여쓰기로 배열에 입력받아 solution에서 두 수의 나눗셈의 나머지가 0이면 1을 아니면 0을 출력

---
+++ CodingTestCollection/Multiple of n/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Multiple_of_n
+{
+    /// <summary>
+    /// 문제 번호 : 181937
+    /// 문제 이름 : n의 배수
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/181937
+    /// </summary>
+    /// 
+    public class Solution
+    {
+        public int solution(int num, int n)
+        {
+            if (num % n == 0)            
+                return 1;           
+            else            
+                return 0;           
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int num = Int32.Parse(s[0]);
+            int n = Int32.Parse(s[1]);
+            Solution answer = new Solution();
+            int result = answer.solution(num, n);
+
+            Console.WriteLine(result);
+        }
+    }
+}
