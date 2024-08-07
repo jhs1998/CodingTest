dad777c0c0383d72074fecf6f5057477fe9ddef4
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-08-07 17:21:45 +0900

    덧셈식 출력하기
    값을 띄어쓰기로 나누어 배열로 입력받아 덧셈식을 출력

---
+++ CodingTestCollection/Additive Output/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Additive_Output
+{
+    /// <summary>
+    /// 문제 번호 : 181947
+    /// 문제 이름 : 덧셈식 출력하기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/181947
+    /// </summary>
+    internal class Program
+    {
+        public static void Main()
+        {
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+            int b = Int32.Parse(s[1]);
+
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+    }
+}
