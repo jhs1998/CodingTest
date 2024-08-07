222f28b0d3c244cbe975e4936ef42c4922ef0f0f
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-08-07 17:28:26 +0900

    문자열 붙여서 출력하기
    String s1과 s2를 +로 연결

---
+++ CodingTestCollection/StringAndPrint/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace StringAndPrint
+{
+    /// <summary>
+    /// 문제 번호 : 181946
+    /// 문제 이름 : 문자열 붙여서 출력하기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/181946
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] input;
+
+            Console.Clear();
+            input = Console.ReadLine().Split(' ');
+
+            String s1 = input[0];
+            String s2 = input[1];
+
+            Console.WriteLine($"{s1 + s2}");
+        }
+    }
+}
