51c8cdbdc8048a1c0f7fdab95c6e63c0a8e9a826
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-08-09 15:18:39 +0900

    문자열 내 마음대로 정렬하기
    문자열로 이루어진 배열을 받아 문자열의 n번째 단어에 따라 오름차순으로 출력하주는 문제.
     n번째 문자를 문자열들의 앞에 넣어준뒤 오름차순으로 정렬해주고 쓰임이 다한 넣어준 문자를 삭제

---
+++ CodingTestCollection/SortStringsAsDesired/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace SortStringsAsDesired
+{
+    ///<summary>
+    ///문제 번호 : 12915
+    ///문제 이름 : 문자열 내 마음대로 정렬하기
+    ///URL : https://school.programmers.co.kr/learn/courses/30/lessons/12915
+    ///</summary>
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] strings = { "sun", "bed", "car" };
+            int n = 1;
+            Solution solution = new Solution();
+            string[] resurts = solution.solution(strings, n);
+            for (int i = 0; i < resurts.Length; i++)
+            {
+                Console.Write($"{resurts[i]} ");
+            }           
+        }
+        // 받아온 배열의 단어들에 n번째 값을 기준으로 오름차순 정리
+        public class Solution
+        {
+            public string[] solution(string[] strings, int n)
+            {
+                int lon = strings.Length;
+                string[] answer = new string[lon];
+
+                for (int i = 0; i < lon; i++)
+                {
+                    //i번째 answer에 n번째 자리 문자를 더해준다
+                    answer[i] = strings[i].Substring(n, 1) + strings[i];
+                }
+                //문자열 정렬
+                Array.Sort(answer);
+
+                for (int i = 0; i < lon; i++)
+                {
+                    //더해준 가장 앞 문자 삭제
+                    answer[i] = answer[i].Remove(0, 1);
+                }
+                return answer;
+            }
+        }
+    }
+}
