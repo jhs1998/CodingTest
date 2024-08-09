b250ace09d08ca93d4c1dd399e2740e0826beaf1
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-08-09 13:03:56 +0900

    가장 큰 수 찾기
    입력한 배열 값을 Solution으로 받아 배열의 가장 큰 수를 찾고 배열에서 그 수가 위치한 순번을 찾아 리턴 해줌.
    그 후 Main에서 리턴 값을 받아 출력
    for문으로 배열을 돌아보며 크기를 비교하여 크면 indexb에 저장
    indexa는 배열의 최댓값 위치를 저장
    answer[] 배열에 indexa, indexa값을 삽입

---
+++ CodingTestCollection/FindLargestNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace FindLargestNumber
+{
+    ///<summary>
+    ///문제 번호 : 120899
+    ///문제 이름 : 가장 큰 수 찾기
+    ///URL : https://school.programmers.co.kr/learn/courses/30/lessons/120899
+    ///</summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 8,2,7,9,181,9,98,7,1,4 };
+            Solution solution = new Solution ();
+            int[] result = solution.solution(array);
+            Console.WriteLine($"최댓값 : {result[0]}, 배열의 {result[1]+1}번째");
+        }
+        public class Solution
+        {
+            public int[] solution(int[] array)
+            {
+                int[] answer = new int[2];
+                int indexa, indexb = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    indexa = array[i];
+                    if (indexb < array[i])
+                    {
+                        indexb = array[i];
+                    }
+                }
+                indexa = Array.IndexOf(array, indexb);
+
+                answer[0] = indexb;
+                answer[1] = indexa;
+
+                return answer;
+            }
+        }
+    }
+}
