09577020995a1d66ae8a3134492e8d95efe4c0d5
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-08-11 20:01:21 +0900

    홀짝 구분하기
    값을 2로 나누어 나머지가 나올경우 {a} is odd출력 나머지가 없을경우 {a} is even 출력

---
+++ CodingTestCollection/EstimatedMatchSheet/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace OddEvenDistinction
+{
+    ///<summary>
+    ///문제 번호 : 181944
+    ///문제 이름 : 홀짝 구분하기
+    ///URL : https://school.programmers.co.kr/learn/courses/30/lessons/181944
+    ///</summary>
+    public class Example
+    {
+        public static void Main()
+        {
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+
+            if (a % 2 == 1)
+            {
+                Console.WriteLine($"{a} is odd");
+            }
+            else if (a % 2 == 0)
+            {
+                Console.WriteLine($"{a} is even");
+            }
+        }
+    }
+}
